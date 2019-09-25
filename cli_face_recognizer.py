5cda293d50303dee916d652c1e5759359dfa9f7b
0-jam <28038981+0-jam@users.noreply.github.com>
2019-09-25 11:56:55 +0900

    JSON output in CLI face recognizer

--- cli_face_recognizer.py
+++ cli_face_recognizer.py
@@ -1,6 +1,7 @@
 import argparse
 import time
 from pathlib import Path
+import json
 
 import cv2
 
@@ -8,8 +9,9 @@ from modules.dark_recognizer import recognize_face
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Recognize objects from an image file without showing any images")
+    parser = argparse.ArgumentParser(description='Recognize objects from an image file without showing any images')
     parser.add_argument('input', type=str, help='Input file')
+    parser.add_argument('--output', '-o', type=str, help='Output file (default: none)')
     args = parser.parse_args()
 
     img = cv2.imread(str(Path(args.input)))
@@ -18,6 +20,14 @@ def main():
     faces = recognize_face(img)
     elapsed_time = time.time() - start_time
     print('Found objects: {}, Elapsed time: {:.2f} sec'.format(len(faces), elapsed_time))
+
+    out_path = args.output
+    if out_path is not None:
+        # Convert numpy.float32 objects to float to save as JSON format
+        [face.update({'confidence': float(face['confidence'])}) for face in faces]
+
+        with Path(out_path).open('w') as out_json:
+            out_json.write(json.dumps(faces, ensure_ascii=False))
 
 
 if __name__ == '__main__':
