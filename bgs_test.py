f64d884f792eff26d3825f042fc69140557e2503
0-jam <28038981+0-jam@users.noreply.github.com>
2020-08-12 15:31:39 +0900

    The simple video difference calculator

--- bgs_test.py
+++ bgs_test.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 
 def main():
@@ -12,11 +13,17 @@ def main():
                 break
 
             fg_mask = bgs.apply(frame)
-            _, th_fg_mask = cv2.threshold(fg_mask, 200, 255, cv2.THRESH_BINARY)
+            _, th_fg_mask = cv2.threshold(fg_mask, 127, 255, cv2.THRESH_BINARY_INV)
 
             cv2.imshow('Lepton Radiometry', frame)
             cv2.imshow('FG Mask', fg_mask)
             cv2.imshow('FG Mask (applied threshold)', th_fg_mask)
+
+            total_pixel = th_fg_mask.shape[0] * th_fg_mask.shape[1]
+            frame_diff = np.count_nonzero(th_fg_mask == 0)
+
+            print('difference: {:5d} / {} ({:.2f}%)'.format(frame_diff, total_pixel, (frame_diff / total_pixel) * 100), end='\r', flush=True)
+
             cv2.waitKey(1)
     except KeyboardInterrupt:
         print()
