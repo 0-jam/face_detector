b7381b66e905834136cf671283dc6410e5267aca
0-jam <28038981+0-jam@users.noreply.github.com>
2020-08-21 13:00:33 +0900

    Added resetting BGS function

--- bgs_test.py
+++ bgs_test.py
@@ -3,12 +3,13 @@ from statistics import mean
 
 import cv2
 
-from modules.bgs_utils import apply_bgs, calc_difference
+from modules.bgs_utils import BGSubtractor
 from modules.resolution import get_video_size
 
 
 def main():
     video = cv2.VideoCapture(0)
+    bgs = BGSubtractor()
 
     video_width, video_height = get_video_size(video)
     total_pixel = video_width * video_height
@@ -21,23 +22,28 @@ def main():
             if ret is None:
                 break
 
-            th_fg_mask = apply_bgs(frame)
+            th_fg_mask = bgs.apply_bgs(frame)
 
             cv2.imshow('Actual Image', frame)
             cv2.imshow('FG Mask (applied threshold)', th_fg_mask)
 
-            frame_diff = calc_difference(th_fg_mask)
+            frame_diff = bgs.calc_difference(th_fg_mask)
 
             diffs.append(frame_diff)
             avg_diff = mean(diffs)
+            avg_diff_rate = (avg_diff / total_pixel) * 100
 
             print('difference: {:5d} / {:9d} ({:.2f}%), average: {:.3f} ({:.2f}%)'.format(
                 frame_diff,
                 total_pixel,
                 (frame_diff / total_pixel) * 100,
                 avg_diff,
-                (avg_diff / total_pixel) * 100,
+                avg_diff_rate,
             ), end='\r', flush=True)
+
+            if avg_diff_rate > 50.0:
+                print('reset bgs')
+                bgs.reset_bgs()
 
             cv2.waitKey(1)
     except KeyboardInterrupt:
--- modules/bgs_utils.py
+++ modules/bgs_utils.py
@@ -2,23 +2,26 @@ import cv2
 import numpy as np
 import tqdm
 
-HISTSIZE = 30 * 60 * 60
-bgs = cv2.createBackgroundSubtractorMOG2(history=HISTSIZE)
 
+class BGSubtractor(object):
+    def __init__(self):
+        self.histsize = 30 * 60 * 60
+        self.bgs = cv2.createBackgroundSubtractorMOG2(history=self.histsize)
 
-def apply_bgs(frame):
-    fg_mask = bgs.apply(frame)
-    _, th_fg_mask = cv2.threshold(fg_mask, 127, 255, cv2.THRESH_BINARY_INV)
+    def apply_bgs(self, frame):
+        fg_mask = self.bgs.apply(frame)
+        _, th_fg_mask = cv2.threshold(fg_mask, 127, 255, cv2.THRESH_BINARY_INV)
 
-    return th_fg_mask
+        return th_fg_mask
 
+    def calc_difference(self, frame):
+        return np.count_nonzero(frame == 0)
 
-def calc_difference(frame):
-    return np.count_nonzero(frame == 0)
+    # Fill history with the current frame
+    # It takes a long time
+    def calibrate_bgs(self, frame):
+        for _ in tqdm.tqdm(range(self.histsize), desc='calibrating bgs ...'):
+            self.apply_bgs(frame)
 
-
-# Fill history with the current frame
-# It takes a long time
-def calibrate_bgs(frame):
-    for _ in tqdm.tqdm(range(HISTSIZE), desc='calibrating bgs ...'):
-        apply_bgs(frame)
+    def reset_bgs(self):
+        self.bgs = cv2.createBackgroundSubtractorMOG2(history=self.histsize)
